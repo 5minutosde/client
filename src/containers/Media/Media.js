c5e708c5fd0384371a7f9c90008a0f5a64ac6cb3
Lucas Inocente <inocente@Inocente.local>
2019-06-25 13:06:10 -0300

    Create query for load single media and apply in the media page

--- src/containers/Media/Media.js
+++ src/containers/Media/Media.js
@@ -1,43 +1,46 @@
 import React, { Component } from "react";
-import axios from 'axios'
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
 import Card from "../../components/Card/Card";
 
-class User extends Component {
+import Database from '../../Database';
 
+class User extends Component {
   constructor(props) {
     super(props)
     this.state = {
       medias: [],
       mediaTitle: 'Loading...',
-      user: 'Loading...'
+      username: 'Loading...'
     }
   }
 
-  componentDidMount() {
-    const { user, mediaSlug } = this.props.match.params
-    axios.get(`https://fiveminutes-5655c.firebaseio.com/audios.json`)
-    .then(res => {
-      const response = Object.keys(res.data).map((key, index) => {
-        return res.data[key]
+  findOneMedia = async (user, mediaSlug) => {
+    return Database.ref('audios')
+    .orderByChild("user/username")
+    .equalTo(user)
+    .once('value')
+    .then(function(snapshot) {
+      const response = snapshot.val() || null;
+      const json = Object.keys(response)
+      const mediaId = json.filter(function(media) {
+        return response[media].slug === mediaSlug
       })
-      const medias = response.filter((media) => {
-        return media.slug === mediaSlug
-      })
-      this.setState({ medias, user, mediaTitle: mediaSlug })
-    })
-    .catch(err => {
-      console.error({ err })
+      return [response[mediaId]]
     })
   }
 
-  render() {
+  async componentDidMount() {
+    const { user, mediaSlug } = this.props.match.params
+    const medias = await this.findOneMedia(user, mediaSlug)
+    this.setState({ medias, username: user, mediaTitle: medias[0].title })
+  }
 
+  render() {
     return (
       <div>
-        <Header user={this.state.user} />
-        <Hero title={`@${this.state.mediaTitle}`} subtitle="Áudios:" />
+        <Header user={this.state.username} />
+        <Hero title={`${this.state.mediaTitle}`} subtitle="Áudios:" />
         <div className="container">
           { this.state.medias.map((media, key) =>
             <Card media={media} key={key} />
